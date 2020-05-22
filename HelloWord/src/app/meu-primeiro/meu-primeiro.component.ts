c844a2747df12202ca7b7c0fdafe48dcb0efb1cd
werley <werley_desouzasilva@hotmail.com>
2020-05-22 13:21:26 -0300

    Componet Hello Word

---
+++ HelloWord/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu primeiro component com Angular 8</p>
+  `
+})
+export class MeuPrimeiroComponent {}
--- TypeScript/src/app/app.module.ts
+++ TypeScript/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
-import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MeuPrimeiroComponent,
-    MeuPrimeiro2Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
